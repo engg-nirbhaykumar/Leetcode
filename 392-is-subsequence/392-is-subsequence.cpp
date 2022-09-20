71d9fbb55ef04ea432061f9abd8e8b2ea1a40534
engg-nirbhaykumar <67179429+engg-nirbhaykumar@users.noreply.github.com>
2022-09-20 22:46:55 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.74%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        //approach 1 using  string library
         // return t.find(s);
+        //approach 2 using simple traversal 
+        //traverse the subseq only if char is matching in both , 
+        //if not matching move further in main string
         int n=s.length(),m=t.length();
-        int j=0;
+        int j=0;//to traverse the subsequence
         for(int i=0;i<m;i++)
         {
-            if(s[j]==t[i])
+            if(s[j]==t[i])//chars are matching
                 j++;
         }
-        return (j==n);
+        return (j==n);//if subseq , all char matched and j must be ==len of subseq
     }
 };
